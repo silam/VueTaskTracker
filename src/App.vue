b71fcb9c35d327deb661e9a28034ec4ee2e972af
Si Lam <silam@hotmail.com>
2021-09-08 20:25:59 -0500

    Change color to green to reminder as TRUE

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container" >
+   <Header title="Task Tracker" />
+   <Tasks  :tasks="tasks"></Tasks>
+</div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Tasks
+  },
+  data() {
+        return {
+            tasks: []
+        }
+    },
+    created() {
+        this.tasks = [
+            {
+                id: 1,
+                text: 'Car Appoitment',
+                day: 'March 19st at 1:00 PM',
+                reminder: true
+            },
+            {
+                id: 2,
+                text: 'Doctors Appoitment',
+                day: 'March 1st at 1:00 PM',
+                reminder: false
+            },
+            {
+                id: 3,
+                text: 'Flu vaccine Appoitment',
+                day: 'March 10st at 1:00 PM',
+                reminder: true
+            }
+        ]
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,43 @@
+<template >
+<div :class="[task.reminder ? 'reminder' : '', 'task']" >
+    <h3>{{task.text}}
+
+        <i class="fas fa-times" />
+    </h3>
+    <p>{{task.day}}</p>
+</div>
+    
+</template>
+
+
+<script>
+
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+        }
+
+}
+</script>
+
+
+<style scoped>
+.fas {
+  color: red;
+}
+.task {
+  background: #746a5181;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
