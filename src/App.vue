677c9833674d434620457e2f675591e42da02a9f
Si Lam <silam@hotmail.com>
2021-09-09 21:38:12 -0500

    Save toggle reminder of a task

--- src/App.vue
+++ src/App.vue
@@ -84,9 +84,22 @@ export default {
 
           }
         },
-      toggleReminder(id) {
+      async toggleReminder(id) {
+
+          const taskToToggle = await this.fetchTask(id)
+          const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+          const res = await fetch(`api/tasks/${id}`, {
+              method: 'PUT',
+              headers:  { 'Content-type': 'application/json'},
+              body: JSON.stringify(updTask),
+
+          })
+
+          const data = await res.json()
+
           this.tasks = this.tasks.map((task)=> task.id === id ? 
-          {...task, reminder : !task.reminder}:task)
+          {...task, reminder : data.reminder }:task)
         },
     },
     async created() {
